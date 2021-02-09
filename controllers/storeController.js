24dce1b1a2a1aa2a47df299d9b2d03ace6eefb29
Madmurt <mccartm6@gmail.com>
2021-02-09 20:55:51 +0000

    adding individual store page

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -53,6 +53,12 @@ exports.getStores = async (req, res) => {
 	res.render('stores', { title: 'Stores', stores });
 };
 
+exports.getStoreBySlug = async (req, res, next) => {
+	const store = await Store.findOne({ slug: req.params.slug });
+	if (!store) return next();
+	res.render('store', { title: store.name, store });
+};
+
 exports.editStore = async (req, res) => {
 	const store = await Store.findById(req.params.id);
 	res.render('editStore', { title: `Edit ${store.name}`, store });
